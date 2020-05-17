8fe1017ee90f2753021635fb33b081e83db90bb1
Shani <p.shani@coti.io>
2020-05-17 13:38:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ const View = styled.div`
   background-size: cover;
 `
 
-
 const App = ({tooltip, toggleTooltip}) => {
 
   useEffect(() => {
@@ -36,8 +35,8 @@ const App = ({tooltip, toggleTooltip}) => {
       {tooltip && <Tooltip/>}
       <Navbar/>
       <Switch>
-        <Route path={`${process.env.PUBLIC_URL}/favorites`} component={Favorites}/>
-        <Route path={`${process.env.PUBLIC_URL}/:key?/:name?`} component={Home}/>
+        <Route path={`/favorites`} component={Favorites}/>
+        <Route path={`/:key?/:name?`} component={Home}/>
       </Switch>
     </View>
   );
